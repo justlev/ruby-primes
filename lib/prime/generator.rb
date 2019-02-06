3d4134831350c57f094b30d417777c83d3c7ccdb
Lev Perlman <levperlman@Levs-MacBook-Pro.local>
2019-02-06 22:43:17 +0200

    comment

--- lib/prime/generator.rb
+++ lib/prime/generator.rb
@@ -1,5 +1,12 @@
 module Prime
   class Generator
+    # Returns an array in the length of {amount} filled with prime numbers, starting from 2.
+    #
+    # I chose this algorithm over the standard Sieve of Eratosthenes on purpose:
+    # This solution is not using pre-processing of an array, so it is not limited to an array size.
+    # Its only limitation is the computational power of your machine + patience.
+    # Since the requirement was to be ready for any length of a matrix,
+    # it might be more efficient to use this technique instead of pre-processing of Eratosthenes array.
     def self.generate_primes(amount = 4)
       primes = []
       return primes if amount <= 0
